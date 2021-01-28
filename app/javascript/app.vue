230bb9240b044efb47b12955768aaa63131858f1
KouSei089 <qq00891829@icloud.com>
2021-01-28 12:07:26 +0900

    Copy

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,17 +3,39 @@
     <Header></Header>
     <FixedNav></FixedNav>
     <router-view></router-view>
+    <ContentView :eList="eList"></ContentView>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
+import ContentView from "./components/ContentView.vue"
 import Header from "./components/Header.vue"
 import FixedNav from "./components/FixedNav.vue"
 
 export default {
   components: {
     Header,
-    FixedNav
-  }
+    FixedNav,
+    ContentView,
+  },
+  el: '#app',
+  data(){
+    return {
+      value: 1,
+      eList: ['', ''],
+    }
+  },
+  mounted() {
+    this.listE();
+  },
+  methods: {
+    listE: function() {
+      axios.get('/api/users')
+      .then(response => {
+        this.eList = response.data
+      });
+    }
+  },
 }
 </script>
--- app/javascript/views/NavHome.vue
+++ app/javascript/views/NavHome.vue
@@ -1,32 +1,3 @@
 <template>
-  <ContentView :eList="eList"></ContentView>
+  <div><h1>Home</h1></div>
 </template>
-
-<script>
-import axios from "axios";
-import ContentView from "./components/ContentView.vue"
-
-export default {
-  components: {
-    ContentView,
-  },
-  el: '#app',
-  data(){
-    return {
-      value: 1,
-      eList: ['', ''],
-    }
-  },
-  mounted() {
-    this.listE();
-  },
-  methods: {
-    listE: function() {
-      axios.get('/api/users')
-      .then(response => {
-        this.eList = response.data
-      });
-    }
-  },
-}
-</script>
