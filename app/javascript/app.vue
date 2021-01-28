e577f0a91e202ac80e96a17b40a102c7a9626c50
KouSei089 <qq00891829@icloud.com>
2021-01-28 09:40:13 +0900

    Props

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,36 @@
 <template>
   <v-app id="app">
     <Header></Header>
-    <ContentView></ContentView>
+    <ContentView :eList="eList"></ContentView>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 import Header from "./components/Header.vue"
 import ContentView from "./components/ContentView.vue"
 
 export default {
   components: {
     Header,
-    ContentView
-  }
+    ContentView,
+  },
+  el: '#app',
+  data(){
+    return {
+      eList: ['', ''],
+    }
+  },
+  mounted() {
+    this.listE();
+  },
+  methods: {
+    listE: function() {
+      axios.get('/api/users')
+      .then(response => {
+        this.eList = response.data
+      });
+    }
+  },
 }
 </script>
-
-<style scoped>
-
-</style>
